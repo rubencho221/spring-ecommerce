88e6a191ba33303eae7eddac0aa491bf4405987a
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-04 11:39:24 -0500

    Funcionalidad guardar producto vista create y logger para la clase productoController

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/ProductoController.java
@@ -1,16 +1,59 @@
 package com.practica.ecommerce.spring_ecommerce.controller;
 
+
+import com.practica.ecommerce.spring_ecommerce.model.Producto;
+
+
+import com.practica.ecommerce.spring_ecommerce.model.Usuario;
+import com.practica.ecommerce.spring_ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping("")
     public String show() {
         return "productos/show";
     }
+
+    @GetMapping("/create")
+    public String create() {
+        return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto) {
+        LOGGER.info("Este es el objeto producto {}", producto);
+
+        Usuario user = Usuario.builder()
+                .id(1)
+                .nombre("")
+                .username("")
+                .email("")
+                .direccion("")
+                .telefono("")
+                .tipo("")
+                .password("")
+                .build();
+
+        producto.setUsuario(user);
+        productoService.save(producto);
+        return "redirect:/productos";
+    }
+
 
 }
--- src/main/java/com/practica/ecommerce/spring_ecommerce/model/Usuario.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/model/Usuario.java
@@ -3,6 +3,7 @@ package com.practica.ecommerce.spring_ecommerce.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
