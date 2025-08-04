cb3e4dfb0f58181605f841eeadff382352571ed9
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-04 18:47:25 -0500

    Desarrollo funcionalidad para eliminar un registro de un producto

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/ProductoController.java
@@ -79,5 +79,11 @@ public class ProductoController {
         return "redirect:/productos";
     }
 
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
+
 
 }
