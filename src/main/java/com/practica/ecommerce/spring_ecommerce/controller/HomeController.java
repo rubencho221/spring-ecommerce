968be3eaa0e16d1cd9a8ecf47d80643d6c4e3a51
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-16 12:28:07 -0500

    Desarrollo funcionalidad para ver reumen de la orden de pedido

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -119,4 +119,9 @@ public class HomeController {
         return "/usuario/carrito";
     }
 
+    @GetMapping("/order")
+    public String order() {
+        return "usuario/resumenorden";
+    }
+
 }
