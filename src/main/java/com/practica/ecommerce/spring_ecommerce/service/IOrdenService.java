053799b134daf63ed45010194b7468346f3bf659
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-16 15:13:24 -0500

    Desarrollo de la funcionalidad para generar el numero de la orden

--- src/main/java/com/practica/ecommerce/spring_ecommerce/service/IOrdenService.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/service/IOrdenService.java
@@ -2,6 +2,9 @@ package com.practica.ecommerce.spring_ecommerce.service;
 
 import com.practica.ecommerce.spring_ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save (Orden orden);
 }
--- src/main/java/com/practica/ecommerce/spring_ecommerce/service/impl/OrdenServiceImpl.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/service/impl/OrdenServiceImpl.java
@@ -6,6 +6,9 @@ import com.practica.ecommerce.spring_ecommerce.service.IOrdenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService {
 
@@ -13,7 +16,36 @@ public class OrdenServiceImpl implements IOrdenService {
     private IOrdenRepository ordenRepository;
 
     @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
     }
+
+    public String generateNumeroOrden() {
+
+        int numero = 0;
+        String numeroConcatenado = "";
+
+        List<Orden> ordenes = findAll();
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add( Integer.parseInt( o.getNumero() )));
+
+        if (ordenes.isEmpty()) {
+            numero = 1;
+        } else {
+            // Obtenemos el ultimo numero de la lista de ordenes
+            numero = numeros.stream().max(Integer::compare).get();
+            numero ++;
+        }
+
+        numeroConcatenado = String.format("%010d", numero);it
+
+        return numeroConcatenado;
+    }
+
 }
