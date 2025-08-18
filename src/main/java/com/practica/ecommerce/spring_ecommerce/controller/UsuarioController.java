abc17f07696b4bf87926e8a3cc315634c87dee2a
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-17 19:21:14 -0500

    Desarrollo funcionalidad para obtener el usuario de sesion

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/UsuarioController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.practica.ecommerce.spring_ecommerce.controller;
 
 import com.practica.ecommerce.spring_ecommerce.model.Usuario;
 import com.practica.ecommerce.spring_ecommerce.service.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -29,6 +32,30 @@ public class UsuarioController {
         logger.info("Usuario registro: {}", usuario);
         usuario.setTipo("USER");
         usuarioService.save(usuario);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session) {
+        logger.info("Accesos : {}", usuario);
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+        //logger.info("Usuario de DB: {}", user.get());
+
+        if (user.isPresent()) {
+            session.setAttribute("idusuario", user.get().getId());
+            if (user.get().getTipo().equals("ADMIN")) {
+                return "redirect:/administrador";
+            } else {
+                return "redirect:/";
+            }
+        } else {
+            logger.info("Usuario no existe");
+        }
         return "redirect:/";
     }
 
