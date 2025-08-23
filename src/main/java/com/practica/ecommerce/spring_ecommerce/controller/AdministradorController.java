6c681d70f57c5cc9c99f9a683c4f05d2791724ad
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-23 16:34:50 -0500

    Desarrollo funcionalidad para ver usuarios en la vista de admin

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/AdministradorController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/AdministradorController.java
@@ -2,6 +2,7 @@ package com.practica.ecommerce.spring_ecommerce.controller;
 
 import com.practica.ecommerce.spring_ecommerce.model.Producto;
 import com.practica.ecommerce.spring_ecommerce.service.IProductoService;
+import com.practica.ecommerce.spring_ecommerce.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ public class AdministradorController {
     @Autowired
     private IProductoService productoService;
 
+    @Autowired
+    private IUsuarioService usuarioService;
+
     @GetMapping("")
     public String home(Model model) {
 
@@ -24,6 +28,14 @@ public class AdministradorController {
         model.addAttribute("productos", productos);
 
         return "administrador/home";
+    }
+
+    @GetMapping("/usuarios")
+    public String usuarios(Model model) {
+
+        model.addAttribute("usuarios", usuarioService.findAll());
+
+        return "administrador/usuarios";
     }
 
 }
--- src/main/java/com/practica/ecommerce/spring_ecommerce/service/IUsuarioService.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/service/IUsuarioService.java
@@ -2,10 +2,12 @@ package com.practica.ecommerce.spring_ecommerce.service;
 
 import com.practica.ecommerce.spring_ecommerce.model.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IUsuarioService {
 
+    List<Usuario> findAll();
     Optional<Usuario> findById(Integer id);
     Usuario save(Usuario usuario);
     Optional<Usuario> findByEmail(String email);
