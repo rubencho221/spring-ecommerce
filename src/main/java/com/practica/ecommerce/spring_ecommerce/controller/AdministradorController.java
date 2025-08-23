74fce02a5460d68757fa9d4f1ed35bd01d88cc65
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-23 17:07:53 -0500

    Desarrollo funcionalidad para ver los detalles de las ordenes desde el ADMIN

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/AdministradorController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/AdministradorController.java
@@ -1,13 +1,17 @@
 package com.practica.ecommerce.spring_ecommerce.controller;
 
+import com.practica.ecommerce.spring_ecommerce.model.Orden;
 import com.practica.ecommerce.spring_ecommerce.model.Producto;
 import com.practica.ecommerce.spring_ecommerce.service.IOrdenService;
 import com.practica.ecommerce.spring_ecommerce.service.IProductoService;
 import com.practica.ecommerce.spring_ecommerce.service.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,6 +28,8 @@ public class AdministradorController {
 
     @Autowired
     private IOrdenService ordenService;
+
+    private Logger logg = LoggerFactory.getLogger(AdministradorController.class);
 
     @GetMapping("")
     public String home(Model model) {
@@ -46,6 +52,18 @@ public class AdministradorController {
 
         model.addAttribute("ordenes", ordenService.findAll());
         return "administrador/ordenes";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalle(Model model, @PathVariable Integer id) {
+
+        logg.info("ID de la Orden {}", id);
+
+        Orden orden = ordenService.findById(id).get();
+
+        model.addAttribute("detalles", orden.getDetalles());
+
+        return "administrador/detalleorden";
     }
 
 }
