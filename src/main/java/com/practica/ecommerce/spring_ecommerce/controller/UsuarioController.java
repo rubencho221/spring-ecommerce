20512160eedcc1feb2fadeecb520b05b42b6a345
Ruben Dario Perez Corredor <rperez@asesoftware.com>
2025-08-23 16:16:39 -0500

    Desarrollo funcionalidad para cerrar la sesion del usuario

--- src/main/java/com/practica/ecommerce/spring_ecommerce/controller/UsuarioController.java
+++ src/main/java/com/practica/ecommerce/spring_ecommerce/controller/UsuarioController.java
@@ -93,4 +93,10 @@ public class UsuarioController {
         return "usuario/detallecompra";
     }
 
+    @GetMapping("/cerrar")
+    public String cerrarSesion(HttpSession session) {
+        session.removeAttribute("idusuario");
+        return "redirect:/";
+    }
+
 }
